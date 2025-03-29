6c032d262207d1c8225a8e11747d26ce84ea507b
Johan Sebastian Herrera Melgarejo <102569088+Cvaslml@users.noreply.github.com>
2025-03-28 19:24:55 -0500

    Add files via upload

---
+++ blog/post/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('categorias/', views.categorias, name='categorias'),
+    path('publicaciones/', views.publicaciones, name='publicaciones'),
+    path('perfil/', views.perfil, name='perfil'),
+]
---
+++ blog/post/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'post/index.html')
+
+def categorias(request):
+    return render(request, 'post/categorias.html')
+
+def publicaciones(request):
+    return render(request, 'post/publicaciones.html')
+
+def perfil(request):
+    return render(request, 'post/perfil.html')
