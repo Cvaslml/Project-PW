a8f59343cc0ec6467d716a3f123a497be9ff9999
Johan Sebastian Herrera Melgarejo <102569088+Cvaslml@users.noreply.github.com>
2025-04-02 18:47:48 -0500

    Update urls.py

--- blog/post/urls.py
+++ blog/post/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('categorias/', views.categorias, name='categorias'),
     path('publicaciones/', views.publicaciones, name='publicaciones'),
-    path('perfil/', views.perfil, name='perfil'),
+    path('perfil', views.perfil, name='perfil'),
+    path('publicaciones/nueva/', views.nueva_publicacion, name='nueva_publicacion'),
+    path('publicaciones/editar/<int:post_id>/', views.editar_publicacion, name='editar_publicacion'),
+    path('publicaciones/eliminar/<int:post_id>/', views.eliminar_publicacion, name='eliminar_publicacion'),
 ]
