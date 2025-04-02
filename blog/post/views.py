f1ac73bbe4099db434fcff685ab56a4db3fe7ac7
Johan Sebastian Herrera Melgarejo <102569088+Cvaslml@users.noreply.github.com>
2025-04-02 18:49:36 -0500

    Update views.py

--- blog/post/views.py
+++ blog/post/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm
 
 # Create your views here.
 
@@ -9,7 +10,41 @@ def categorias(request):
     return render(request, 'post/categorias.html')
 
 def publicaciones(request):
-    return render(request, 'post/publicaciones.html')
+    if request.user.is_superuser or request.user.is_staff:
+        posts = Post.objects.all().order_by('-created_at')  # Muestra todos los posts
+    else:
+        posts = Post.objects.filter(author=request.user).order_by('-created_at')  # Muestra solo los del usuario actual
+    return render(request, 'post/publicaciones.html', {'posts': posts})
 
 def perfil(request):
     return render(request, 'post/perfil.html')
+
+def nueva_publicacion(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)  # No guarda aún en la BD
+            post.author = User.objects.get(id=1)  # Asigna el usuario autenticado
+            post.save()  # Guarda en la BD
+            return redirect('publicaciones')  # Redirigir después de guardar
+    else:
+        form = PostForm()
+    return render(request, 'post/nueva_publicacion.html', {'form': form})
+
+def editar_publicacion(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('publicaciones')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'post/editar_publicacion.html', {'form': form})
+
+def eliminar_publicacion(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('publicaciones')
+    return render(request, 'post/borrar_publicacion.html', {'post': post})
