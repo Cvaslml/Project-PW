a0852fb1130ecfb036c73f9fcd094ba38e387b21
Johan Sebastian Herrera Melgarejo <102569088+Cvaslml@users.noreply.github.com>
2025-04-16 13:15:57 -0500

    Delete invpro directory

--- invpro/productos/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Producto
-# Register your models here.
-
-class ProductoAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'precio', 'cantidad')
-
-admin.site.register(Producto, ProductoAdmin)
--- invpro/productos/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    return render(request, 'productos/index.html')
-
-def perfil(request):
-    return render(request, 'productos/perfil.html')
